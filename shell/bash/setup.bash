99ca3bdc32a33fdd3e065c366bcea94dd45d5b2f
EthRousseau <ethanrousseau99+github@gmail.com>
2025-01-31 17:09:05 -0500

    WIP

--- shell/bash/setup.bash
+++ shell/bash/setup.bash
@@ -1,55 +1,53 @@
 #!/bin/zsh
 
 if [[ "$SHELLY" == "" ]]; then
-    echo "\$SHELLY is not set, load the shelly environment first"
-    exit 1
+	echo "\$SHELLY is not set, load the shelly environment first"
+	exit 1
 fi
 
 # Make sure to source shelly when new shell is launched
 if [ ! -f ~/.bashrc ]; then
-    echo "making ~/.bashrc"
+	echo "making ~/.bashrc"
 
-	echo "export SHELL_NAME=bash" > ~/.bashrc
-    echo "source $SHELLY/shell/common/auto-loader.bash" >> ~/.bashrc
+	echo "export SHELL_NAME=bash" >~/.bashrc
+	echo "source $SHELLY/shell/common/auto-loader.bash" >>~/.bashrc
 fi
 
 isMac=false
 if [[ "$(uname)" == "Darwin" ]]; then
-    isMac=true
+	isMac=true
 fi
 
 if ! which cargo &>/dev/null; then
-    echo "Installing Rust"
-    curl https://sh.rustup.rs -sSf | sh
+	echo "Installing Rust"
+	curl https://sh.rustup.rs -sSf | sh
 fi
 
 # Install homebrew
 if $isMac && ! which brew &>/dev/null; then
-    echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    brew install eza btop neovim nvm wezterm ripgrep
+	echo "Installing homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if [ ! -d ~/.config ]; then
-    echo "making ~/.config"
-    mkdir ~/.config
+	echo "making ~/.config"
+	mkdir ~/.config
 fi
 
 rm -rf ~/.config/nvim
 ln -s $SHELLY/nvim ~/.config/nvim
 
 linkConfigFile() {
-    src=$1
-    dest=$2
+	src=$1
+	dest=$2
 
-    if [[ ! -e $dest ]] || [[ "$(/bin/ls -i $src | awk '{print $1}')" != "$(/bin/ls -i $dest | awk '{print $1}')" ]]; then
-        echo "Linking $dest..."
-        rm -f $dest
-        mkdir -p $(dirname $dest)
-        ln $src $dest
+	if [[ ! -e $dest ]] || [[ "$(/bin/ls -i $src | awk '{print $1}')" != "$(/bin/ls -i $dest | awk '{print $1}')" ]]; then
+		echo "Linking $dest..."
+		rm -f $dest
+		mkdir -p $(dirname $dest)
+		ln $src $dest
 
-    fi
+	fi
 }
 
 tmuxName=tmux-$(uname).conf
@@ -65,5 +63,5 @@ weztermPath=~/.config/wezterm/wezterm.lua
 linkConfigFile $SHELLY/wezterm/$weztermName $weztermPath
 
 if [[ $TMUX != "" ]]; then
-    tmux source-file $tmuxPath
+	tmux source-file $tmuxPath
 fi
