56726aef7e899f3fe8823b88f831ba76ab7e4208
Ethan Rousseau <erousseau@ptc.com>
2026-04-07 22:52:00 -0400

    feat(nvim): migrate movement plugins (leap, nvim-jump, tmux-nav, lastplace) to vim.pack

--- nvim/lua/plugins_old/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"farmergreg/vim-lastplace", -- Remember last cursor place
-
-	-- <C-{h/j/k/l}> between panes. Don't remove, even though no longer using tmux
-	"christoomey/vim-tmux-navigator",
-}
